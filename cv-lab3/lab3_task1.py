091d9425d656fbe595383fada22ce88ae415fbcd
TroddenSpade <troddenspade@gmail.com>
2021-03-11 11:33:36 +0330

    lab3 updated

--- cv-lab3/lab3_task1.py
+++ cv-lab3/lab3_task1.py
@@ -7,26 +7,32 @@ cap = cv2.VideoCapture('eggs.avi')
 # get the dimensions of the frame
 # you can also read the first frame to get these
 w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # width of the frame
-h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) # height of the frame
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # height of the frame
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID') # choose codec
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  # choose codec
 
 # opencv 2.x:
-#w = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)) 
-#h = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)) 
+#w = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+#h = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
 #fourcc = cv2.cv.CV_FOURCC(*'XVID')
 
 # create VideoWriter object w by h, 30 frames per second
-out = cv2.VideoWriter('eggs-reverse.avi',fourcc, 30.0, (w,h))
+out = cv2.VideoWriter('eggs-reverse.avi', fourcc, 30.0, (w, h))
+
+buffer = []
 
 while True:
     ret, I = cap.read()
 
-    if ret == False: # end of video (or error)
+    if ret == False:  # end of video (or error)
         break
 
     # write the current frame I
+    buffer.append(I)
+
+for I in reversed(buffer):
     out.write(I)
+
 
 cap.release()
 out.release()
--- cv-lab3/lab3_task2.py
+++ cv-lab3/lab3_task2.py
@@ -2,18 +2,51 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-fname = 'crayfish.jpg'
-#fname = 'office.jpg'
+fnames = ['crayfish.jpg', 'office.jpg', 'map.jpg', 'train.jpg', 'terrain.jpg']
 
-I = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
+for fname in fnames:
+    I = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
+    f, axes = plt.subplots(2, 3)
 
-f, axes = plt.subplots(2, 3)
+    axes[0, 0].imshow(I, 'gray', vmin=0, vmax=255)
+    axes[0, 0].axis('off')
 
-axes[0,0].imshow(I, 'gray', vmin=0, vmax=255)
-axes[0,0].axis('off')
+    axes[1, 0].hist(I.ravel(), 256, [0, 256])
 
-axes[1,0].hist(I.ravel(),256,[0,256]);
+    # second column
+    pixels = I.ravel()
+    hist, bins = np.histogram(pixels, 256, [0, 256])
+    cdf = hist.cumsum()
+    cdf_m = np.ma.masked_equal(cdf, 0)
+    # print(cdf_m)
+    cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
+    print(cdf_m.min(), cdf.max())
+    cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+    # print(cdf)
 
-plt.show()
+    J = cdf[I]
+    # a = np.where(cdf == 0)[0][-1]
+    # b = np.where(cdf == 255)[0][0]
+    # # a = pixels.mean() - 2 * pixels.std()
+    # # b = pixels.mean() + 2 * pixels.std()
+    # print(a, b)
 
+    # J = (I-a) * 255.0 / (b-a)
+    # J[J < 0] = 0
+    # J[J > 255] = 255
+    # J = J.astype(np.uint8)
 
+    axes[0, 1].imshow(J, 'gray', vmin=0, vmax=255)
+    axes[0, 1].axis('off')
+
+    axes[1, 1].hist(J.ravel(), 256, [0, 256])
+
+    # third column
+    K = cv2.equalizeHist(I)
+
+    axes[0, 2].imshow(K, 'gray', vmin=0, vmax=255)
+    axes[0, 2].axis('off')
+
+    axes[1, 2].hist(K.ravel(), 256, [0, 256])
+
+    plt.show()
